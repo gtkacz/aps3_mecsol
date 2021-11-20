29d8b42f6c4f38a2e607076b2f9bcb0a474e969a
Tkacz <gabrielmt2@al.insper.edu.br>
2021-11-20 08:26:21 -0300

    att

---
+++ main.py
@@ -0,0 +1,11 @@
+from funcoesTermosol import *
+import numpy as np
+
+def main():
+    nn, N, nm, Inc, nc, F, nr, R = importa('entrada.xlsx')
+    
+    E = Inc[0,2]
+    A = Inc[0,3]
+    
+if __name__ == '__main__':
+    main()
